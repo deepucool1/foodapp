384b7545b6d6b82da963ff7ed54628c50dc52eb6
deepak <dmor83@gmail.com>
2020-04-18 00:19:40 +0530

    added params

--- src/screens/searchscreen.js
+++ src/screens/searchscreen.js
@@ -1,15 +1,34 @@
 import React, { useState } from 'react'
 import { View, StyleSheet, Text } from 'react-native'
 import SearchBar from "../components/searchbar";
+import yelp from '../api/yelp'
+
+
 
 const searchscreen = () => {
     const [term, setTerm] = useState('')
+    const [results, setResults] = useState([])
+
+    const searchApi = async () => {
+        const response = await yelp.get('/search', {
+            params: {
+                limit: 50,
+                term,
+                location: 'san jose'
+            }
+        })
+        setResults(response.data.businesses)
+    }
+
+
     return (
         <View style={styles.view}>
             <SearchBar
                 term={term}
                 onTermChange={setTerm}
+                onTermSubmit={searchApi}
             />
+            <Text>we have found {results.length} results</Text>
         </View>
     )
 }
